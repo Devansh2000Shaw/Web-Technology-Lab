633032e4fb31a5dc415c2abb73c8bd7174a678b8
Devansh2000Shaw <akashshaw03680@gmail.com>
2021-08-10 01:18:45 +0530

    Question 5

---
+++ Lab 4 Java/ques5.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class ques5 {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your first name and last name: ");
+        String FirstName = sc.nextLine();
+        String LastName = sc.nextLine();
+        System.out.println("FirstName:"+FirstName +" " + "LastName:"+LastName);
+    }
+}
+
