b388799a7c9422e0efaedf7daa733f4b650b4d79
Devansh2000Shaw <akashshaw03680@gmail.com>
2021-08-10 01:17:29 +0530

    Question 1

---
+++ Lab 4 Java/ques1.java
@@ -0,0 +1,8 @@
+public class ques1 {
+    public static void main(String[] args)
+    {
+        System.out.println("Devansh Shaw");
+        System.out.println(1905605);
+        System.out.println("CSE");
+    }
+}
